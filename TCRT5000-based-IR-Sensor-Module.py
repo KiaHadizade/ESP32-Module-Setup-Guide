f169e5a7131d8a36dd2730f75552f0c45101758e
Kia Hadizade <kiahadizade3077@gmail.com>
2025-08-02 19:44:46 +0330

    Now it looks better

--- TCRT5000-based-IR-Sensor-Module.py
+++ TCRT5000-based-IR-Sensor-Module.py
@@ -1,6 +1,5 @@
 # ========================= "TCRT5000 Infrared Reflective Line Track Sensor Module" ========================= #
 
-
 from machine import Pin
 import time
 
@@ -8,30 +7,11 @@ import time
 ir_sensor = Pin(23, Pin.IN)
 
 while True:
-    if ir_sensor.value() == 0:
-        print("Object detected!")
-    else:
-        print("No object.")
+    print("IR raw value:", ir_sensor.value())
     time.sleep(0.5)
 
-
-
-
-
-
-
-
-
-
-# ================= test =================
-
-# from machine import Pin
-# import time
-
-# ir_sensor = Pin(23, Pin.IN)
-
-# while True:
-#     print("IR raw value:", ir_sensor.value())
-#     time.sleep(0.5)
-
-# ================= test =================
+    # if ir_sensor.value() == 0:
+    #     print("Object detected!")
+    # else:
+    #     print("No object.")
+    # time.sleep(0.5)
